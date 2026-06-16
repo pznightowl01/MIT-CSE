234ad339d4ec7978fc29c9e184ae8400cde076a5
Princita Zina Miranda <159634545+pznightowl01@users.noreply.github.com>
2026-06-16 12:24:34 +0530

    Add files via upload

---
+++ oslab/oslab4/q3.c
@@ -0,0 +1,9 @@
+#include "headers.h"
+int main(){
+    int a = 8;
+    float b = 8.8;
+    char c = 'x';
+    char s[] = "shiv";
+    printf("int = %d\nfloat = %f\nchar = %c\nstring = %s\nhex for int = %x\nexp for float = %e\n", a, b, c, s, a, b);
+    return 0;
+} 
