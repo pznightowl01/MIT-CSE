e6459697060d5eaee90937a89f5334f4efcc8f83
Princita Zina Miranda <159634545+pznightowl01@users.noreply.github.com>
2026-07-08 17:52:20 +0530

    Add files via upload

--- ppl/lab5/q1pb.cu
+++ ppl/lab5/q1pb.cu
@@ -2,14 +2,14 @@
 //Section : SCE-C
 //Roll no. C17, Reg. no. 220905115
 
-//Q)Write a program in CUDA to add two vectors of length N using block size as N
+//Q)Write a program in CUDA to add two vectors of length N using N threads
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <stdio.h>
 
 __global__ void vecadd(int *A,int *B,int *C){
-    int index=blockIdx.x*blockDim.x+threadIdx.x;
+    int index=threadIdx.x;
     C[index]=A[index]+B[index];
     }
 
@@ -37,8 +37,8 @@ int main(){
     cudaMemcpy(A,a,n*sizeof(int),cudaMemcpyHostToDevice);
     cudaMemcpy(B,b,n*sizeof(int),cudaMemcpyHostToDevice);
 
-    dim3 grid(n,1,1);
-    dim3 blk(1,1,1);
+    dim3 grid(1,1,1);
+    dim3 blk(n,1,1);
 
     vecadd<<<grid,blk>>>(A,B,C);
     cudaMemcpy(c,C,n*sizeof(int),cudaMemcpyDeviceToHost);
