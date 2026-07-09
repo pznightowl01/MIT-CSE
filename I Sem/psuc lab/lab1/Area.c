7d65c6fd8adcbfa2123c2d080cfef30628e24be2
Princita Zina Miranda <159634545+pznightowl01@users.noreply.github.com>
2026-07-09 17:17:34 +0530

    Add files via upload

--- I Sem/psuc lab/lab1/Area.c
+++ I Sem/psuc lab/lab1/Area.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+int main()
+{
+    int C,F;
+    printf("Enter F");
+    scanf("%d%d", &C, &F);
+    C=5/9(F-32)
+    printf("Temperature in Centigrade=%d",C);
+    return 0;
+}
