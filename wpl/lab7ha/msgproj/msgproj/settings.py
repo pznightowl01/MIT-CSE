e94ccf3bf42529d3a7caf6be563ba538c15f14a0
Princita Zina Miranda <159634545+pznightowl01@users.noreply.github.com>
2026-07-09 11:52:14 +0530

    Create settings.py

---
+++ wpl/lab7ha/msgproj/msgproj/settings.py
@@ -0,0 +1,23 @@
+INSTALLED_APPS = [
+'msgapp',
+'django.contrib.admin',
+'django.contrib.auth',
+'django.contrib.contenttypes',
+'django.contrib.sessions',
+'django.contrib.messages',
+'django.contrib.staticfiles',
+]
+TEMPLATES = [
+{
+'BACKEND': 'django.template.backends.django.DjangoTemplates',
+'DIRS': [],
+'APP_DIRS': True,
+'OPTIONS': {
+'context_processors': [
+'django.template.context_processors.request',
+'django.contrib.auth.context_processors.auth',
+'django.contrib.messages.context_processors.messages',
+],
+},
+},
+]
