838a01b1e87e3f87463e6d0f058dbdbb4ada12de
Princita Zina Miranda <159634545+pznightowl01@users.noreply.github.com>
2026-07-09 16:46:07 +0530

    Update q1in.c

--- cd-lab/lab3/q1in.c
+++ cd-lab/lab3/q1in.c
@@ -1,19 +1,7 @@
-#include <stdio.h>
-
 main()
 {
 int a,b,sum;
-a += 1;
+a = 1;
 b = 1;
 sum = a + b;
-printf("sum",sum);
-break;
-
-if(a<=b) 
-printf("done"); //don't miss printf
-return -1
-
-continue;
-break;
 }
-
